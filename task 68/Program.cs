fb8f631248d9ee07fa1a748bdd1184e8069fad07
Orange <dunduk201@yandex.ru>
2022-11-25 19:24:50 +0900

    task 68 was fixed

--- task 68/Program.cs
+++ task 68/Program.cs
@@ -8,3 +8,15 @@ int a = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите натуральное число: ");
 int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"A({a}, {b}) = {funcAkkerman(a, b)}");
+
+int funcAkkerman(int a, int b)
+{
+    if (a == 0)
+        return b + 1;
+    if (a > 0 && b == 0)
+        return funcAkkerman(a - 1, 1);
+    else
+        return funcAkkerman(a - 1, funcAkkerman(a, b - 1));
+}
